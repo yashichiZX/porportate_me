f912f3a567d96a1d4a0b58edcafd97231d8ceddb
itsumoonazicode <dyingkouki@gmail.com>
2017-06-18 13:49:15 +0900

    大まかなデザイン案の落とし込み

--- wp/wp-content/themes/main_theme/header.php
+++ wp/wp-content/themes/main_theme/header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>My portfolio</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
 <?php wp_head(); ?>
 </head>
 <body>
