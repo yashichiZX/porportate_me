810a540d2f959b665d7920ce82585b77555844d3
itsumoonazicode <dyingkouki@gmail.com>
2017-06-20 01:22:16 +0900

    ベースデザイン整備

--- wp/wp-content/themes/main_theme/index.php
+++ wp/wp-content/themes/main_theme/index.php
@@ -2,14 +2,16 @@
 
 <!--*********** content **************-->
 
+<img class="liner" src="<?php echo get_stylesheet_directory_uri(); ?>/img/line.png">
+
 <!-- 全体 -->
 <div class="wholeDesign">
 	<!-- 横一列分 -->
 	<div id="inlineDesign">
 		<div id="contentA_Z">
 			<p class="a_to_z">A</p>
-			<h5>AKAGAKI - YA</h5>
-			<h2>赤垣屋</h2>
+			<h5 id="subtitle">AKAGAKI - YA</h5>
+			<h2 id="maintitle">赤垣屋</h2>
 			<p id="content_text">
 				川端二条の居酒屋「赤垣屋」へ行くなら、夏の晴れた日と決めている。
 				開店の少し前に並んでカウンターの一番入り口に近い席を確保。斜めに差し込む西日の美しい眩しさよ。
@@ -18,8 +20,8 @@
 
 		<div id="contentA_Z">
 			<p class="a_to_z">A</p>
-			<h5>AKAGAKI - YA</h5>
-			<h2>赤垣屋</h2>
+			<h5 id="subtitle">AKAGAKI - YA</h5>
+			<h2 id="maintitle">赤垣屋</h2>
 			<p id="content_text">
 				川端二条の居酒屋「赤垣屋」へ行くなら、夏の晴れた日と決めている。
 				開店の少し前に並んでカウンターの一番入り口に近い席を確保。斜めに差し込む西日の美しい眩しさよ。
@@ -28,8 +30,8 @@
 
 		<div id="contentA_Z">
 			<p class="a_to_z">A</p>
-			<h5>AKAGAKI - YA</h5>
-			<h2>赤垣屋</h2>
+			<h5 id="subtitle">AKAGAKI - YA</h5>
+			<h2 id="maintitle">赤垣屋</h2>
 			<p id="content_text">
 				川端二条の居酒屋「赤垣屋」へ行くなら、夏の晴れた日と決めている。
 				開店の少し前に並んでカウンターの一番入り口に近い席を確保。斜めに差し込む西日の美しい眩しさよ。
@@ -38,13 +40,13 @@
 
 		<div id="contentA_Z">
 			<p class="a_to_z">A</p>
-			<h5>AKAGAKI - YA</h5>
-			<h2>赤垣屋</h2>
+			<h5 id="subtitle">AKAGAKI - YA</h5>
+			<h2 id="maintitle">赤垣屋</h2>
 			<p id="content_text">
 				川端二条の居酒屋「赤垣屋」へ行くなら、夏の晴れた日と決めている。
 				開店の少し前に並んでカウンターの一番入り口に近い席を確保。斜めに差し込む西日の美しい眩しさよ。
 			</p>
-		</div>	
+		</div>
 	</div><!-- inlineDesign -->
 
 
@@ -52,8 +54,8 @@
 	<div id="inlineDesign">
 		<div id="contentA_Z">
 			<p class="a_to_z">A</p>
-			<h5>AKAGAKI - YA</h5>
-			<h2>赤垣屋</h2>
+			<h5 id="subtitle">AKAGAKI - YA</h5>
+			<h2 id="maintitle">赤垣屋</h2>
 			<p id="content_text">
 				川端二条の居酒屋「赤垣屋」へ行くなら、夏の晴れた日と決めている。
 				開店の少し前に並んでカウンターの一番入り口に近い席を確保。斜めに差し込む西日の美しい眩しさよ。
@@ -62,8 +64,8 @@
 
 		<div id="contentA_Z">
 			<p class="a_to_z">A</p>
-			<h5>AKAGAKI - YA</h5>
-			<h2>赤垣屋</h2>
+			<h5 id="subtitle">AKAGAKI - YA</h5>
+			<h2 id="maintitle">赤垣屋</h2>
 			<p id="content_text">
 				川端二条の居酒屋「赤垣屋」へ行くなら、夏の晴れた日と決めている。
 				開店の少し前に並んでカウンターの一番入り口に近い席を確保。斜めに差し込む西日の美しい眩しさよ。
@@ -72,8 +74,8 @@
 
 		<div id="contentA_Z">
 			<p class="a_to_z">A</p>
-			<h5>AKAGAKI - YA</h5>
-			<h2>赤垣屋</h2>
+			<h5 id="subtitle">AKAGAKI - YA</h5>
+			<h2 id="maintitle">赤垣屋</h2>
 			<p id="content_text">
 				川端二条の居酒屋「赤垣屋」へ行くなら、夏の晴れた日と決めている。
 				開店の少し前に並んでカウンターの一番入り口に近い席を確保。斜めに差し込む西日の美しい眩しさよ。
@@ -82,13 +84,13 @@
 
 		<div id="contentA_Z">
 			<p class="a_to_z">A</p>
-			<h5>AKAGAKI - YA</h5>
-			<h2>赤垣屋</h2>
+			<h5 id="subtitle">AKAGAKI - YA</h5>
+			<h2 id="maintitle">赤垣屋</h2>
 			<p id="content_text">
 				川端二条の居酒屋「赤垣屋」へ行くなら、夏の晴れた日と決めている。
 				開店の少し前に並んでカウンターの一番入り口に近い席を確保。斜めに差し込む西日の美しい眩しさよ。
 			</p>
-		</div>	
+		</div>
 	</div><!-- inlineDesign -->
 
 <!-- 横一列分 -->
